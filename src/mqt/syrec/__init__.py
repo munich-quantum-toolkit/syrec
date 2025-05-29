e84a50341e43e91bc7bd9d3ebc2cfd7f2c785a2d
Fabian Hingerl <fabian.hingerl@gmail.com>
2025-05-29 17:31:09 +0200

    Attempt to fix build error on windows due to failure to import dll

--- src/mqt/syrec/__init__.py
+++ src/mqt/syrec/__init__.py
@@ -14,6 +14,20 @@ See README.md or go to https://github.com/cda-tum/syrec for more information.
 
 from __future__ import annotations
 
+# under Windows, make sure to add the appropriate DLL directory to the PATH
+if sys.platform == "win32":
+    def _dll_patch() -> None:
+        """Add the DLL directory to the PATH."""
+        import os
+        import sysconfig
+        from pathlib import Path
+
+        bin_dir = Path(sysconfig.get_paths()["purelib"]) / "mqt" / "core" / "bin"
+        os.add_dll_directory(str(bin_dir))
+
+    _dll_patch()
+    del _dll_patch
+
 from ._version import version as __version__
 from .pysyrec import (
     annotatable_quantum_computation,
