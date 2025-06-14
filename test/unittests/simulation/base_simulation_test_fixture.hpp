bea3d8acd9604b7d0b42ebb65e9ad491a6aeb2d4
Fabian Hingerl <fabian.hingerl@gmail.com>
2025-06-14 13:24:18 +0200

    Added typed test fixture to unify full circuit simulation test and remove previous test fixtures

---
+++ test/unittests/simulation/base_simulation_test_fixture.hpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+ * Copyright (c) 2025 Munich Quantum Software Company GmbH
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Licensed under the MIT License
+ */
+
+#pragma once
+#include "algorithms/simulation/simple_simulation.hpp"
+#include "algorithms/synthesis/syrec_cost_aware_synthesis.hpp"
+#include "algorithms/synthesis/syrec_line_aware_synthesis.hpp"
+#include "core/n_bit_values_container.hpp"
+#include "core/annotatable_quantum_computation.hpp"
+#include "core/syrec/program.hpp"
+
+#include <cstddef>
+#include <gtest/gtest.h>
+#include <string>
+
+template<typename T>
+class BaseSimulationTestFixture: public ::testing::Test {
+public:
+    void SetUp() override {
+        static_assert(std::is_same_v<T, syrec::CostAwareSynthesis> || std::is_same_v<T, syrec::LineAwareSynthesis>);
+    }
+
+    [[nodiscard]] virtual std::string getPathToTestCaseDataDirectory() = 0;
+
+    void assertSimulationResultForStateMatchesExpectedOne(const syrec::NBitValuesContainer& inputState, const syrec::NBitValuesContainer& expectedOutputState) const {
+        ASSERT_EQ(inputState.size(), expectedOutputState.size());
+
+        syrec::NBitValuesContainer actualOutputState(inputState.size());
+        ASSERT_NO_FATAL_FAILURE(syrec::simpleSimulation(actualOutputState, annotatableQuantumComputation, inputState));
+        ASSERT_EQ(actualOutputState.size(), expectedOutputState.size());
+
+        // We are assuming that the indices of the ancilla qubits are larger than the one of the inputs/output qubits.
+        const std::size_t numQubitsToCheck = annotatableQuantumComputation.getNqubitsWithoutAncillae();
+        for (std::size_t i = 0; i < numQubitsToCheck; ++i) {
+            ASSERT_EQ(expectedOutputState[i], actualOutputState[i]) << "Value mismatch during simulation at qubit " << std::to_string(i) << ", expected: " << std::to_string(static_cast<int>(expectedOutputState[i])) << " but was " << std::to_string(static_cast<int>(actualOutputState[i]))
+                                                                    << "!\nInput state: " << inputState.stringify() << " | Expected output state: " << expectedOutputState.stringify() << " | Actual output state: " << actualOutputState.stringify();
+        }
+    }
+
+    [[nodiscard]] bool performProgramSynthesis(const syrec::Program& program) {
+        if (std::is_same_v<T, syrec::CostAwareSynthesis>) {
+            return syrec::CostAwareSynthesis::synthesize(annotatableQuantumComputation, program);
+        }
+        return syrec::LineAwareSynthesis::synthesize(annotatableQuantumComputation, program);
+    }
+
+    syrec::AnnotatableQuantumComputation annotatableQuantumComputation;
+    syrec::Program                       syrecProgramInstance;
+};
---
+++ test/unittests/simulation/test_synthesized_full_circuit_simulation.cpp
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+ * Copyright (c) 2025 Munich Quantum Software Company GmbH
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Licensed under the MIT License
+ */
+
+#include "base_simulation_test_fixture.hpp"
+#include "algorithms/synthesis/syrec_cost_aware_synthesis.hpp"
+#include "algorithms/synthesis/syrec_line_aware_synthesis.hpp"
+#include "core/n_bit_values_container.hpp"
+#include "core/syrec/program.hpp"
+
+#include <cstddef>
+#include <fstream>
+#include <gtest/gtest.h>
+#include <nlohmann/json.hpp>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <vector>
+
+// .clang-tidy reports a false positive here since are including the required nlohman json header file
+using json = nlohmann::json; // NOLINT(misc-include-cleaner)
+
+using namespace syrec;
+
+namespace {
+    template<typename T>
+    class FullCircuitSimulationTestsFixture: public BaseSimulationTestFixture<T> {
+    public:
+        void performTestExecutionForCircuit(const std::string& inputCircuitFilename, const std::string& inputCircuitJsonKeyInConfig) {
+            const std::string pathToInputCircuit = getPathToTestCaseDataDirectory() + inputCircuitFilename;
+
+            std::string errorsOfReadInputCircuit;
+            ASSERT_NO_FATAL_FAILURE(errorsOfReadInputCircuit = this->syrecProgramInstance.read(pathToInputCircuit, syrec::ReadProgramSettings()));
+            ASSERT_TRUE(errorsOfReadInputCircuit.empty()) << "Expected no errors in input circuits but actually found the following: " << errorsOfReadInputCircuit;
+
+            ASSERT_TRUE(this->performProgramSynthesis(this->syrecProgramInstance)) << "Synthesis of input circuit was not successful";
+
+            NBitValuesContainer inputState(this->annotatableQuantumComputation.getNqubits());
+            expectedOutputState.resize(inputState.size());
+
+            ASSERT_NO_FATAL_FAILURE(this->loadTestCaseDataFromJson(inputCircuitJsonKeyInConfig));
+            ASSERT_NO_FATAL_FAILURE(this->prepareInputState(inputState));
+            this->assertSimulationResultForStateMatchesExpectedOne(inputState, this->expectedOutputState);
+        }
+
+    protected:
+        [[nodiscard]] std::string getPathToTestCaseDataDirectory() override {
+            return "../circuits/";
+        }
+
+        [[nodiscard]] static std::optional<std::string> getPathToTestCaseConfig() {
+            if (std::is_same_v<T, LineAwareSynthesis>) {
+                return "../configs/circuits_line_aware_simulation.json";
+            }
+            if (std::is_same_v<T, CostAwareSynthesis>) {
+                return "../configs/circuits_cost_aware_simulation.json";
+            }
+            return std::nullopt;
+        }
+
+        static void loadNBitValuesContainerFromString(NBitValuesContainer& resultContainer, const std::string_view& stringifiedContainerContent) {
+            for (std::size_t i = 0; i < stringifiedContainerContent.size(); ++i) {
+                if (stringifiedContainerContent[i] == '1') {
+                    ASSERT_TRUE(resultContainer.flip(i)) << "Failed to flip value for output bit " << std::to_string(i);
+                }
+            }
+        }
+
+        void loadTestCaseDataFromJson(const std::string& jsonKeyForCircuitInConfig) {
+            const std::optional<std::string> pathToTestCaseConfig = getPathToTestCaseConfig();
+            ASSERT_TRUE(pathToTestCaseConfig.has_value()) << "Path to test case configuration was not defined for given synthesizer type";
+
+            std::ifstream testCaseConfigInputFilestream(*pathToTestCaseConfig);
+            json          fullTestCaseConfigJson = json::parse(testCaseConfigInputFilestream);
+            ASSERT_TRUE(fullTestCaseConfigJson.contains(jsonKeyForCircuitInConfig));
+
+            const auto& currTestCaseConfigJson = fullTestCaseConfigJson[jsonKeyForCircuitInConfig];
+            ASSERT_TRUE(currTestCaseConfigJson.is_structured()) << "Configuration entry of circuit '" << jsonKeyForCircuitInConfig << "' must be a structured entry!";
+
+            const std::string setInputIndicesJsonKey = "set_lines";
+            ASSERT_TRUE(currTestCaseConfigJson.contains(setInputIndicesJsonKey)) << "Configuration did not contain expected entry '" << setInputIndicesJsonKey << "'";
+            ASSERT_TRUE(currTestCaseConfigJson[setInputIndicesJsonKey].is_array()) << "Set input lines must be defined in the json as an integer array";
+            setInputIndices = currTestCaseConfigJson[setInputIndicesJsonKey].get<std::vector<int>>();
+
+            const std::string expectedOutputStateJsonKey = "sim_out";
+            ASSERT_TRUE(currTestCaseConfigJson.contains(expectedOutputStateJsonKey)) << "Configuration did not contain expected entry '" << expectedOutputStateJsonKey << "'";
+            ASSERT_TRUE(currTestCaseConfigJson[expectedOutputStateJsonKey].is_string()) << "Expected output state must be defined as a binary string";
+            ASSERT_NO_FATAL_FAILURE(loadNBitValuesContainerFromString(expectedOutputState, currTestCaseConfigJson[expectedOutputStateJsonKey]));
+        }
+
+        void prepareInputState(NBitValuesContainer& inputState) const {
+            for (const auto& setInputIndex: setInputIndices) {
+                ASSERT_GE(setInputIndex, 0) << "Index of input bit must be non-negative integer but was actually " << std::to_string(setInputIndex);
+                ASSERT_TRUE(inputState.set(static_cast<std::size_t>(setInputIndex))) << "Could not set value of input " << std::to_string(setInputIndex) << " in input state";
+            }
+        }
+
+        std::vector<int>    setInputIndices;
+        NBitValuesContainer expectedOutputState;
+    };
+} // namespace
+
+TYPED_TEST_SUITE_P(FullCircuitSimulationTestsFixture);
+
+TYPED_TEST_P(FullCircuitSimulationTestsFixture, TestOfCircuitAlu2) {
+    this->performTestExecutionForCircuit("alu_2.src", "alu_2");
+}
+
+TYPED_TEST_P(FullCircuitSimulationTestsFixture, TestOfCircuitSwap2) {
+    this->performTestExecutionForCircuit("swap_2.src", "swap_2");
+}
+
+TYPED_TEST_P(FullCircuitSimulationTestsFixture, TestOfCircuitSimpleAdd2) {
+    this->performTestExecutionForCircuit("simple_add_2.src", "simple_add_2");
+}
+
+TYPED_TEST_P(FullCircuitSimulationTestsFixture, TestOfCircuitMultiply2) {
+    this->performTestExecutionForCircuit("multiply_2.src", "multiply_2");
+}
+
+TYPED_TEST_P(FullCircuitSimulationTestsFixture, TestOfCircuitModulo2) {
+    this->performTestExecutionForCircuit("modulo_2.src", "modulo_2");
+}
+
+TYPED_TEST_P(FullCircuitSimulationTestsFixture, TestOfCircuitNegate8) {
+    this->performTestExecutionForCircuit("negate_2.src", "negate_2");
+}
+
+REGISTER_TYPED_TEST_SUITE_P(FullCircuitSimulationTestsFixture,
+                            TestOfCircuitAlu2,
+                            TestOfCircuitSwap2,
+                            TestOfCircuitSimpleAdd2,
+                            TestOfCircuitMultiply2,
+                            TestOfCircuitModulo2,
+                            TestOfCircuitNegate8);
+
+using SynthesizerTypes = testing::Types<CostAwareSynthesis, LineAwareSynthesis>;
+INSTANTIATE_TYPED_TEST_SUITE_P(SyrecSynthesisTest, FullCircuitSimulationTestsFixture, SynthesizerTypes, );
