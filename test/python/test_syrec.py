152e30c520de6f95ebbc1e955136e9597ba97013
TooMuchDakka <fabian.hingerl@gmail.com>
2025-06-22 21:45:42 +0200

    Updatd simulation tests in python tests to use .json data in unified format

--- test/python/test_syrec.py
+++ test/python/test_syrec.py
@@ -17,8 +17,9 @@ import pytest
 from mqt import syrec
 
 test_dir = Path(__file__).resolve().parent.parent
-configs_dir = test_dir / "configs"
 circuit_dir = test_dir / "circuits"
+configs_dir = test_dir / "configs"
+simulation_test_data_dir = test_dir / "unittests" / "simulation" / "data"
 
 
 @pytest.fixture
@@ -35,14 +36,35 @@ def data_cost_aware_synthesis():
 
 @pytest.fixture
 def data_line_aware_simulation():
-    with (configs_dir / "circuits_line_aware_simulation.json").open() as f:
+    with (simulation_test_data_dir / "test_line_aware_synthesis_of_full_circuits.json").open() as f:
         return json.load(f)
 
 
 @pytest.fixture
 def data_cost_aware_simulation():
-    with (configs_dir / "circuits_cost_aware_simulation.json").open() as f:
+    with (simulation_test_data_dir / "test_cost_aware_synthesis_of_full_circuits.json").open() as f:
         return json.load(f)
+
+
+def init_n_bit_values_container_with_expected_state(
+    state_container: syrec.n_bit_values_container, stringified_state: str
+) -> None:
+    assert len(stringified_state) <= state_container.size()
+    for idx, stringified_qubit_value in enumerate(stringified_state):
+        assert stringified_qubit_value in {"0", "1"}
+        if stringified_qubit_value == "1":
+            state_container.set(idx)
+
+
+def compare_some_values_of_n_bit_values_container(
+    n: int, expected: syrec.n_bit_values_container, actual: syrec.n_bit_values_container
+) -> None:
+    assert n > 0
+    assert n <= expected.size()
+    assert expected.size() == actual.size()
+
+    for i in range(n):
+        assert expected[i] == actual[i]
 
 
 def test_parser(data_line_aware_synthesis: dict[str, Any]) -> None:
@@ -94,53 +116,49 @@ def test_synthesis_add_lines(data_cost_aware_synthesis: dict[str, Any]) -> None:
 
 
 def test_simulation_no_lines(data_line_aware_simulation: dict[str, Any]) -> None:
-    for file_name in data_line_aware_simulation:
+    for test_case_name in data_line_aware_simulation:
         annotatable_quantum_computation = syrec.annotatable_quantum_computation()
         prog = syrec.program()
-        error = prog.read(str(circuit_dir / (file_name + ".src")))
+        errors = prog.read_from_string(data_line_aware_simulation[test_case_name]["inputCircuit"])
 
-        assert not error
+        assert not errors
         assert syrec.line_aware_synthesis(annotatable_quantum_computation, prog)
 
-        input_qubit_values = syrec.n_bit_values_container(annotatable_quantum_computation.num_qubits)
-        set_list = data_line_aware_simulation[file_name]["set_lines"]
+        for simulation_run_data in data_line_aware_simulation[test_case_name]["simulationRuns"]:
+            input_state = syrec.n_bit_values_container(annotatable_quantum_computation.num_qubits)
+            expected_output_state = syrec.n_bit_values_container(input_state.size())
+            actual_output_state = syrec.n_bit_values_container(input_state.size())
 
-        for set_index in set_list:
-            input_qubit_values.set(set_index)
+            init_n_bit_values_container_with_expected_state(input_state, simulation_run_data["in"])
+            init_n_bit_values_container_with_expected_state(expected_output_state, simulation_run_data["out"])
 
-        output_qubit_values = syrec.n_bit_values_container()
-        syrec.simple_simulation(output_qubit_values, annotatable_quantum_computation, input_qubit_values)
-        assert input_qubit_values.size() == output_qubit_values.size()
-
-        for i in range(output_qubit_values.size()):
-            actual_qubit_value = "1" if output_qubit_values[i] is True else "0"
-            expected_qubit_value = data_line_aware_simulation[file_name]["sim_out"][i]
-            assert expected_qubit_value == actual_qubit_value
+            syrec.simple_simulation(actual_output_state, annotatable_quantum_computation, input_state)
+            compare_some_values_of_n_bit_values_container(
+                annotatable_quantum_computation.num_data_qubits, expected_output_state, actual_output_state
+            )
 
 
 def test_simulation_add_lines(data_cost_aware_simulation: dict[str, Any]) -> None:
-    for file_name in data_cost_aware_simulation:
+    for test_case_name in data_cost_aware_simulation:
         annotatable_quantum_computation = syrec.annotatable_quantum_computation()
         prog = syrec.program()
-        error = prog.read(str(circuit_dir / (file_name + ".src")))
+        errors = prog.read_from_string(data_cost_aware_simulation[test_case_name]["inputCircuit"])
 
-        assert not error
+        assert not errors
         assert syrec.cost_aware_synthesis(annotatable_quantum_computation, prog)
 
-        input_qubit_values = syrec.n_bit_values_container(annotatable_quantum_computation.num_qubits)
-        set_list = data_cost_aware_simulation[file_name]["set_lines"]
+        for simulation_run_data in data_cost_aware_simulation[test_case_name]["simulationRuns"]:
+            input_state = syrec.n_bit_values_container(annotatable_quantum_computation.num_qubits)
+            expected_output_state = syrec.n_bit_values_container(input_state.size())
+            actual_output_state = syrec.n_bit_values_container(input_state.size())
 
-        for set_index in set_list:
-            input_qubit_values.set(set_index)
+            init_n_bit_values_container_with_expected_state(input_state, simulation_run_data["in"])
+            init_n_bit_values_container_with_expected_state(expected_output_state, simulation_run_data["out"])
 
-        output_qubit_values = syrec.n_bit_values_container()
-        syrec.simple_simulation(output_qubit_values, annotatable_quantum_computation, input_qubit_values)
-        assert input_qubit_values.size() == output_qubit_values.size()
-
-        for i in range(output_qubit_values.size()):
-            actual_qubit_value = "1" if output_qubit_values[i] is True else "0"
-            expected_qubit_value = data_cost_aware_simulation[file_name]["sim_out"][i]
-            assert expected_qubit_value == actual_qubit_value
+            syrec.simple_simulation(actual_output_state, annotatable_quantum_computation, input_state)
+            compare_some_values_of_n_bit_values_container(
+                annotatable_quantum_computation.num_data_qubits, expected_output_state, actual_output_state
+            )
 
 
 def test_no_lines_to_qasm(data_line_aware_synthesis: dict[str, Any]) -> None:
