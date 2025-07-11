d722d78a9c5b48a4fa8c4141c2cc05988e2ee25e
Daniel Haag <daniel.haag@tum.de>
2025-07-11 12:06:01 +0200

    Also bump minimum version

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -41,7 +41,7 @@ if(BUILD_MQT_SYREC_BINDINGS)
 endif()
 
 # cmake-format: off
-set(MQT_CORE_MINIMUM_VERSION 3.0.3
+set(MQT_CORE_MINIMUM_VERSION 3.1.0
     CACHE STRING "MQT Core minimum version")
 set(MQT_CORE_VERSION 3.1.0
     CACHE STRING "MQT Core version")
