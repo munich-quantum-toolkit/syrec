1bc85660df6fcb12efc12c4da4c6b7adfeb1a0ff
Daniel Haag <daniel.haag@tum.de>
2025-07-11 15:03:55 +0200

    Bump required version of pybind11 everywhere

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -37,7 +37,7 @@ if(BUILD_MQT_SYREC_BINDINGS)
   endif()
 
   # add pybind11 library
-  find_package(pybind11 2.13.6 CONFIG REQUIRED)
+  find_package(pybind11 3.0.0 CONFIG REQUIRED)
 endif()
 
 # cmake-format: off
