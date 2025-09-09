8b007b711f9fdcca01fdc50a05133b1d72640996
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2025-09-09 20:38:13 +0200

    Remove artifacts of old organization

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -47,12 +47,12 @@ set(MQT_CORE_VERSION 3.2.1
     CACHE STRING "MQT Core version")
 set(MQT_CORE_REV "0425f88169f573e4505b49703c4cadf3699ccbcd"
     CACHE STRING "MQT Core identifier (tag, branch or commit hash)")
-set(MQT_CORE_REPO_OWNER "cda-tum"
+set(MQT_CORE_REPO_OWNER "munich-quantum-toolkit"
 	CACHE STRING "MQT Core repository owner (change when using a fork)")
 # cmake-format: on
 FetchContent_Declare(
   mqt-core
-  GIT_REPOSITORY https://github.com/${MQT_CORE_REPO_OWNER}/mqt-core.git
+  GIT_REPOSITORY https://github.com/${MQT_CORE_REPO_OWNER}/core.git
   GIT_TAG ${MQT_CORE_REV}
   FIND_PACKAGE_ARGS ${MQT_CORE_MINIMUM_VERSION})
 list(APPEND FETCH_PACKAGES mqt-core)
