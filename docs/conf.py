9529d9ba05498823e2d8e00a04aa4893c73b8547
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2026-01-08 23:16:10 +0100

    Update license headers

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
-# Copyright (c) 2025 Munich Quantum Software Company GmbH
+# Copyright (c) 2023 - 2026 Chair for Design Automation, TUM
+# Copyright (c) 2025 - 2026 Munich Quantum Software Company GmbH
 # All rights reserved.
 #
 # SPDX-License-Identifier: MIT
@@ -48,9 +48,9 @@ if TYPE_CHECKING:
 # -- Project information -----------------------------------------------------
 
 project = "MQT SyReC Synthesizer"
-author = "Lukas Burgholzer"
+author = "Chair for Design Automation, TUM & Munich Quantum Software Company"
 language = "en"
-project_copyright = "Chair for Design Automation, Technical University of Munich"
+project_copyright = "2023 - 2026 Chair for Design Automation, TUM & 2025 - 2026 Munich Quantum Software Company"
 
 # -- General configuration ---------------------------------------------------
 
